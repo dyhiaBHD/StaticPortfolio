22a0ea5c591e62edde5bedb8fed4355ea2545c63
dyhiaBHD <bouhamdanidyhia@gmail.com>
2022-12-17 12:43:55 -0500

    adjusting scroll bar

---
+++ JS/scroll.js
@@ -0,0 +1,12 @@
+/************ https://codepen.io/arnedecant/pen/OeZJqQ arne decant **************/
+const progressbar = document.querySelector('progress');
+let isScrolling = false;
+document.addEventListener('scroll', e => isScrolling = true);
+render();
+
+function render() {
+    requestAnimationFrame(render);
+    if (!isScrolling) return;
+    progressbar.value = window.scrollY / (document.body.offsetHeight - window.innerHeight) * 100;
+    isScrolling = false;
+}
